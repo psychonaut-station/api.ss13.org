3a20e57b53bbf258dd3d3c4c90c8ae032912df12
Sefa <50076109+Seefaaa@users.noreply.github.com>
2024-03-22 21:34:52 +0300

    Cors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use rocket::{get, routes, Config as RocketConfig};
+use rocket::{get, http::Method, routes, Config as RocketConfig};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions, Error as CorsError};
 use thiserror::Error;
 
 use crate::config::Config;
@@ -19,10 +20,11 @@ async fn main() -> Result<(), Error> {
     let rocket_config = RocketConfig {
         address: config.address,
         port: config.port,
-        ..RocketConfig::default()
+        ..Default::default()
     };
 
     let rocket = rocket::custom(rocket_config)
+        .attach(cors()?)
         .manage(config)
         .mount("/", routes![index]);
 
@@ -33,9 +35,29 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
+fn cors() -> Result<Cors, CorsError> {
+    let allowed_origins = AllowedOrigins::some_regex(&["^https?://.+"]);
+    let allowed_methods = vec![Method::Get].into_iter().map(From::from).collect();
+    let allowed_headers =
+        AllowedHeaders::some(&["Accept", "Authorization", "Content-Type", "X-CSRF-Token"]);
+    let expose_headers = vec!["Link".to_string()].into_iter().collect();
+
+    CorsOptions {
+        allowed_origins,
+        allowed_methods,
+        allowed_headers,
+        expose_headers,
+        allow_credentials: false,
+        max_age: Some(300),
+        ..Default::default()
+    }
+    .to_cors()
+}
+
 #[derive(Debug, Error)]
 #[error(transparent)]
 enum Error {
     Config(#[from] config::Error),
+    Cors(#[from] rocket_cors::Error),
     Rocket(#[from] rocket::Error),
 }
