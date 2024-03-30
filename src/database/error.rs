881c510ede990fb314d9cc356070cc7391810e27
Seefaaa <sefa@outlook.ie>
2024-03-30 18:51:24 +0300

    Unverify

--- src/database/error.rs
+++ src/database/error.rs
@@ -6,10 +6,12 @@ pub enum Error {
     Sqlx(#[from] sqlx::Error),
     #[error("Player not found")]
     PlayerNotFound,
-    #[error("No ckey or id provided")]
-    NoCkeyOrId,
+    #[error("Invalid arguments provided")]
+    InvalidArguments,
     #[error("Discord account is already linked to {0}")]
     AlreadyLinked(String),
+    #[error("Account is not linked")]
+    NotLinked,
     #[error("Token does not exist or is invalid")]
     TokenInvalid,
     #[error("Token is already in use by {0}")]
--- src/database/verify.rs
+++ src/database/verify.rs
@@ -19,9 +19,11 @@ pub async fn verify_discord(
         return Err(Error::TokenInvalid);
     }
 
-    get_discord_id(one_time_token, false, &mut connection).await?;
+    get_discord_id_by_token(one_time_token, false, &mut connection).await?;
 
-    if let Ok(Some(discord_id)) = get_discord_id(one_time_token, true, &mut connection).await {
+    if let Ok(Some(discord_id)) =
+        get_discord_id_by_token(one_time_token, true, &mut connection).await
+    {
         return Err(Error::TokenInUse(discord_id));
     }
 
@@ -34,6 +36,48 @@ pub async fn verify_discord(
     let ckey = get_ckey(discord_id, &mut connection).await?;
 
     Ok(ckey.unwrap())
+}
+
+pub async fn unverify_discord(
+    discord_id: Option<&str>,
+    ckey: Option<&str>,
+    pool: &MySqlPool,
+) -> Result<String, Error> {
+    let mut connection = pool.acquire().await?;
+
+    if discord_id.is_some() {
+        let discord_id = discord_id.unwrap();
+        let ckey = get_ckey(discord_id, &mut connection).await?;
+
+        if ckey.is_some() {
+            let query =
+                query("UPDATE discord_links SET valid = 0 WHERE discord_id = ? AND valid = 1");
+            let query = query.bind(discord_id);
+
+            connection.execute(query).await?;
+
+            return Ok(ckey.unwrap());
+        }
+
+        return Err(Error::NotLinked);
+    } else if ckey.is_some() {
+        let ckey = ckey.unwrap();
+        let discord_id = get_discord_id_by_ckey(ckey, &mut connection).await?;
+
+        if discord_id.is_some() {
+            let query =
+                query("UPDATE discord_links SET valid = 0 WHERE LOWER(ckey) = ? AND valid = 1");
+            let query = query.bind(ckey.to_lowercase());
+
+            connection.execute(query).await?;
+
+            return Ok(format!("@{}", discord_id.unwrap()));
+        }
+
+        return Err(Error::NotLinked);
+    }
+
+    Err(Error::InvalidArguments)
 }
 
 async fn get_ckey(
@@ -50,7 +94,21 @@ async fn get_ckey(
     Ok(None)
 }
 
-async fn get_discord_id(
+async fn get_discord_id_by_ckey(
+    ckey: &str,
+    connection: &mut PoolConnection<MySql>,
+) -> Result<Option<i64>, Error> {
+    let query = query("SELECT discord_id FROM discord_links WHERE LOWER(ckey) = ? AND valid = 1");
+    let bound = query.bind(ckey.to_lowercase());
+
+    if let Ok(row) = connection.fetch_one(bound).await {
+        return Ok(row.try_get("discord_id").ok());
+    }
+
+    Ok(None)
+}
+
+async fn get_discord_id_by_token(
     one_time_token: &str,
     only_valid: bool,
     connection: &mut PoolConnection<MySql>,
