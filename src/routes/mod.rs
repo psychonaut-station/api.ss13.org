349567072fa9a98f96485ed3e2a2ab833bd63f08
Sefa <sefa@outlook.ie>
2024-05-05 21:48:00 +0300

    Rename `test_merges` module to `recent_test_merges`

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,9 @@
 use rocket::{routes, Build, Rocket};
 
-mod test_merges;
+mod recent_test_merges;
 mod v2;
 
 pub fn mount(rocket: Rocket<Build>) -> Rocket<Build> {
-    let rocket = rocket.mount("/", routes![test_merges::recent_test_merges]);
+    let rocket = rocket.mount("/", routes![recent_test_merges::recent_test_merges]);
     v2::mount(rocket)
 }
