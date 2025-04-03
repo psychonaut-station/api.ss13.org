90a36cf2243eb6745c7d3c59d36e2eba49c17aaa
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-04-03 13:43:51 +0000

    feat: implement basic API with Poem and add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{Route, Server as PoemServer, listener::TcpListener};
+use poem_openapi::{OpenApi, OpenApiService, param::Query, payload::PlainText};
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/hello", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}!", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let api = OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000/api");
+    let ui = api.stoplight_elements();
+    let app = Route::new().nest("/api", api).nest("/", ui);
+
+    PoemServer::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await
 }
